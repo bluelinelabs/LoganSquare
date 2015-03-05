12d3f4faf05e186cf9415ee4870b74a3c1177e03
Conor O'Donnell <mrconor@gmail.com>
2015-03-05 20:56:06 +0000

    Fixed bar chart indices

--- BenchmarkDemo/app/src/main/java/com/bluelinelabs/logansquare/demo/MainActivity.java
+++ BenchmarkDemo/app/src/main/java/com/bluelinelabs/logansquare/demo/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends ActionBarActivity {
         } else if (parser instanceof JacksonDatabindParser) {
             mBarChart.addTiming(section, 1, parseResult.runDuration / 1000f);
         } else if (parser instanceof LoganSquareParser) {
-            mBarChart.addTiming(section, 4, parseResult.runDuration / 1000f);
+            mBarChart.addTiming(section, 2, parseResult.runDuration / 1000f);
         }
     }
 
@@ -175,7 +175,7 @@ public class MainActivity extends ActionBarActivity {
         } else if (serializer instanceof JacksonDatabindSerializer) {
             mBarChart.addTiming(section, 1, serializeResult.runDuration / 1000f);
         } else if (serializer instanceof LoganSquareSerializer) {
-            mBarChart.addTiming(section, 4, serializeResult.runDuration / 1000f);
+            mBarChart.addTiming(section, 2, serializeResult.runDuration / 1000f);
         }
     }
 
