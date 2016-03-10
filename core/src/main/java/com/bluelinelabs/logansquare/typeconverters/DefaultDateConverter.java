db88169626ca9008d836afdc6470e759bf663bf6
Eric Kuck <eric@bluelinelabs.com>
2016-03-10 10:19:26 -0600

    Fixed issue with the default date converter reusing date formatters across threads

--- core/src/main/java/com/bluelinelabs/logansquare/typeconverters/DefaultDateConverter.java
+++ core/src/main/java/com/bluelinelabs/logansquare/typeconverters/DefaultDateConverter.java
@@ -5,14 +5,8 @@ import java.text.DateFormat;
 /** The default DateTypeConverter implementation. Attempts to parse ISO8601-formatted dates. */
 public class DefaultDateConverter extends DateTypeConverter {
 
-    private DateFormat mDateFormat;
-
-    public DefaultDateConverter() {
-        mDateFormat = new DefaultDateFormatter();
-    }
-
     public DateFormat getDateFormat() {
-        return mDateFormat;
+        return new DefaultDateFormatter();
     }
 
 }
